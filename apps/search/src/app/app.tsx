4259d22a2b22a86535d43f1c8906da8c0540889e
arumugasundar <arumugasundar29@gmail.com>
2022-06-08 09:39:13 +0530

    micro frontend based setup

---
+++ apps/search/src/app/app.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import NxWelcome from './nx-welcome';
+import { Account } from '@mf-react-1/shared-data-access';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+
+export function App() {
+  const [name,setName] = useState<string>(Account.name);
+
+  useEffect(() => {
+    setName(() => {
+      Account.name = 'Everyone';
+      return Account.name;
+    })
+  },[]);
+
+  return (
+    <>
+      <NxWelcome title={name} />
+      <div />
+    </>
+  );
+}
+
+export default App;
